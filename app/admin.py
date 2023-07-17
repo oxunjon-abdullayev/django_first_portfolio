a88eb043ee8c59bc0498fb8b0e80da108c686791
oxunjon <abdullayevoxunjon757@gmail.com>
2023-07-17 17:58:30 +0500

    commit boldi

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from app.models import Skill, Blog, Contact
 
-admin.site.register(Skill)
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_diplay = ['image', 'title', 'description', 'user']
+
+
+class SkillAdmin(admin.ModelAdmin):
+    list_diplay = ['title', 'description']
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Skill, SkillAdmin)
 admin.site.register(Contact)
+
+
